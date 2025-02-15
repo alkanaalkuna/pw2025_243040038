64f84fde5b0cb7bbad0de1fbb12f9572a3fe55e7
alkanaalkuna <regantraalkxen@gmail.com>
2025-02-15 18:35:03 +0700

    first commit

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,10 @@
+<?php
+$namaLengkap = "Derania Safa";
+$email = "zainaderaniasafa@gmail.com";
+$pekerjaan = "mahasiswa";
+$jurusan = "Teknik Informatika";
+$universitas = "Universitas Pasundan";
+echo "<h1>$namaLengkap</h1>";
+echo "<h4>$email</h4>";
+echo "<p>Halo nama saya $namaLengkap. Saya adalah $pekerjaan dari $jurusan di $universitas </p>";
+?>
