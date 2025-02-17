4e021b93ccc595979477a10acf319caee1da9be1
alkanaalkuna <regantraalkxen@gmail.com>
2025-02-17 12:22:10 +0700

    tugas 1 pw

---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,14 @@
+<?php
+$angka = 38;
+
+echo "Aku adalah angka $angka<br>";
+$angka *= 5;
+echo "Jika aku dikali 5, maka aku sekarang menjadi <b>$angka</b><br>";
+$angka /= 2;
+echo "Jika aku dibagi 2, maka aku sekarang menjadi <b>$angka</b><br>";
+$angka += 75;
+echo "Jika aku ditambah 75, maka aku sekarang menjadi <b>$angka</b><br>";
+$angka -= 20;
+echo "Jika aku dikurangi 20, maka aku sekarang menjadi <b>$angka</b><br>";
+
+?>
---
+++ tugas/tugas1/1c.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1c</title>
+    <style>
+        .row {
+            display: flex;
+            justify-content: flex-start;
+            margin-bottom: 5px;
+        }
+        .box {
+            width: 50px;
+            height: 50px;
+            background-color: tomato;
+            color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: 2px solid black;
+            margin-right: 5px;
+            font-size: 20px;
+            font-weight: bold;
+        }
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+
+<div class="container">
+    <?php
+    for ($i = 1; $i <= 3; $i++) {
+        echo '<div class="row">';
+        for ($j = 1; $j <= $i; $j++) {
+            echo "<div class='box'>$j</div>";
+        }
+        echo '</div>';
+    }
+    ?>
+</div>
+
+</body>
+</html>
